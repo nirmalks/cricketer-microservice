ce987033c819df431519373b11a172ab26333953
Nirmal Kumar Sekar <kagosekar@gmail.com>
2021-12-11 09:13:35 +0530

    Replace url with service name in feign client

--- cricketer-service/src/main/kotlin/com/example/feignclients/TeamFeignClient.kt
+++ cricketer-service/src/main/kotlin/com/example/feignclients/TeamFeignClient.kt
@@ -6,8 +6,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(url = "\${team.service.url}", value = "team-feign-client",
-        path = "/api/teams")
+@FeignClient(value = "team-service", path = "/api/teams")
 interface TeamFeignClient {
     @GetMapping("/{id}", consumes = ["application/json"])
     fun getTeam(@PathVariable("id") id: String): Team
